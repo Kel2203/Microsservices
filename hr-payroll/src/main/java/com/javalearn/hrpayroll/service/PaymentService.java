ea34984f27519bc8457fe108ad0d31eee9c38e44
kel2203 <raquelsb2001@gmail.com>
2023-06-15 13:48:24 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/javalearn/hrpayroll/service/PaymentService.java
@@ -0,0 +1,12 @@
+package com.javalearn.hrpayroll.service;
+
+
+import com.javalearn.hrpayroll.entities.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+public Payment getPayment(long workerId, int days){
+    return new Payment("Bob", 200.0, days);
+}
+}
