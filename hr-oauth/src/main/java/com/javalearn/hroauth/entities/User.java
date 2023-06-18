c58988ebf8ebd9e8e3d55476d1e2b517a183d4b7
kel2203 <raquelsb2001@gmail.com>
2023-06-18 19:03:47 -0300

    Login, JWT token

--- hr-oauth/src/main/java/com/javalearn/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/javalearn/hroauth/entities/User.java
@@ -1,81 +1,54 @@
 package com.javalearn.hroauth.entities;
 
 
-import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
-
+import java.util.stream.Collectors;
 
 public class User implements Serializable {
-    private static final long serialVersionUID = 1L;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+    public class User implements UserDetails, Serializable {
+        private static final long serialVersionUID = 1L;
 
-    private Long id;
-    private String name;
+        private Long id;
+        @@ -85,4 +91,35 @@ public boolean equals(Object obj) {
+            return false;
+            return true;
+        }
 
-    private String email;
-    private String password;
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return roles.stream().map(x -> new SimpleGrantedAuthority(x.getRoleName()))
+                    .collect(Collectors.toList());
+        }
 
+        @Override
+        public String getUsername() {
+            return email;
+        }
 
-    public User(){
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
 
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
     }
-
-    public User(Long id, String name, String email, String password) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-}
--- hr-oauth/src/main/java/com/javalearn/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/javalearn/hroauth/services/UserService.java
@@ -1,24 +1,42 @@
 package com.javalearn.hroauth.services;
-
 import com.javalearn.hroauth.entities.User;
 import com.javalearn.hroauth.feignclients.UserFeignClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class UserService {
-    private static Logger logger = LoggerFactory.getLogger(UserService.class);
-    @Autowired
-    private UserFeignClient userFeignClient;
+    public class UserService implements UserDetailsService {
 
-    public User findByEmail(String email){
-        User user = userFeignClient.findByEmail(email).getBody();
-        if(user == null){
-            logger.error("Email not found" + email);
-            throw new IllegalArgumentException("Email not found!");
+        private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
+        @Autowired
+        private UserFeignClient userFeignClient;
+
+        public User findByEmail(String email) {
+            User user = userFeignClient.findByEmail(email).getBody();
+            if (user == null) {
+                logger.error("Email not found: " + email);
+                throw new IllegalArgumentException("Email not found");
+            }
+            logger.info("Email found: " + email);
+            return user;
         }
-        logger.info("Email found: " + email);
-        return user;
+
+        @Override
+        public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+            User user = userFeignClient.findByEmail(username).getBody();
+            if (user == null) {
+                logger.error("Email not found: " + username);
+                throw new UsernameNotFoundException("Email not found");
+            }
+            logger.info("Email found: " + username);
+            return user;
+        }
     }
-}
