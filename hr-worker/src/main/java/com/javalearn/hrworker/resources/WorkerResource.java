df709413b9b178b6d8bceddb0796279dc2cd38fc
kel2203 <raquelsb2001@gmail.com>
2023-06-21 14:46:12 -0300

    container references

--- hr-worker/src/main/java/com/javalearn/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/javalearn/hrworker/resources/WorkerResource.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,9 @@ import java.util.List;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment env;
     @Autowired
     private WorkerRepository repository;
 
@@ -36,7 +40,9 @@ public class WorkerResource {
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
 
-       Worker obj = repository.findById(id).get();
+        logger.info("PORT = " + env.getProperty("local.server.port"));
+
+        Worker obj = repository.findById(id).get();
         return ResponseEntity.ok(obj);
     }
 }
